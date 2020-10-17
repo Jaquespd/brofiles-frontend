160d1ac3bac106ad5605b905025ef19451c5e7a2
Jaquespd <jaquespd@gmail.com>
2020-10-17 06:38:00 -0300

    change ip api

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:3000"
+  // baseURL: "http://localhost:3000"
+  baseURL: "https://brofiles-backend-s47ww.ondigitalocean.app/brofiles"
 });
 
 export default api;
